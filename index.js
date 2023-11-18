f670357ad571697d56b58bd5134e534cb9d273b7
Anson Ash <ansonjash@gmail.com>
2023-11-17 21:15:27 -0700

    form submission

--- index.js
+++ index.js
@@ -13,8 +13,30 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/browse', function(req, res) {
-    res.render('browse')
+app.get('/browse', function (req, res) {
+    res.render('browse');
+});
+
+app.get('/share', function (req, res) {
+    res.render('share');
+});
+
+app.post('/share', function (req, res) {
+    const filePath = path.join(__dirname, 'data', 'games.json')
+    const fileData = fs.readFileSync(filePath)
+    const storedGame = JSON.parse(fileData)
+    const gameData = req.body;
+
+    storedGame.push(gameData)
+
+    fs.writeFileSync(filePath, JSON.stringify(storedGame))
+
+    res.redirect('/confirm')
 })
+
+app.get('/confirm', function(req, res) {
+    res.render('confirm');
+})
+
 
 app.listen(3000);
